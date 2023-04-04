bf620fd65838024a81718d18006d4fb12487c119
vinit <vinitkkulkarni@gmail.com>
2023-04-04 22:37:02 +0530

    Added:RollDice using random

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game");
         System.out.println("Initial position : " + playerPosition);
+        int diceValue = (int)(Math.random()*10)%6+1;
+        System.out.println("Dice Value = " + diceValue);
     }
 }
