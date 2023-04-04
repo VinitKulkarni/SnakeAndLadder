f970858b7e728a751eed26e161befe6704f6746a
vinit <vinitkkulkarni@gmail.com>
2023-04-04 22:21:15 +0530

    Added:Inital position=0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.Day4PP1;
 
 public class SnakeAndLadder {
+    static int playerPosition = 0;
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game");
+        System.out.println("Initial position : " + playerPosition);
     }
 }
